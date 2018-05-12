40c9cc1c414d71a0e311f7d7311e124e7ded3cee
Chris Powell <chris.ian.powell@gmail.com>
2018-05-12 16:34:48 -0600

    key generator finished test written for enigma

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -1,31 +1,16 @@
 require_relative 'test_helper'
 require './lib/key_generator'
+
 class KeyGeneratorTest < Minitest::Test
   def test_key_generator_exists
     key_gen = KeyGenerator.new
+
     assert_instance_of KeyGenerator, key_gen
   end
 
   def test_key
     key_gen = KeyGenerator.new
+
     assert key_gen.key < 100000 && key_gen.key > 9999
-  end
-
-  def test_date
-    key_gen = KeyGenerator.new
-
-    assert_equal '05-11-2018', key_gen.date
-  end
-
-  def test_split_key_into_four_ints
-    key_gen = KeyGenerator.new
-
-    assert_equal 4, key_gen.split_key.length
-  end
-
-  def test_each_split_key_is_between_0_and_100
-    key_gen = KeyGenerator.new
-
-    assert key_gen.split
   end
 end
