7e9f548283ded57908594c016ccf5d638fd29b9a
Chris Powell <chris.ian.powell@gmail.com>
2018-05-12 10:01:44 -0600

    initial branch push

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -9,6 +9,10 @@ class KeyGeneratorTest < Minitest::Test
   def test_key
     key_gen = KeyGenerator.new
     assert key_gen.key < 100000 && key_gen.key > 9999
+  end
 
+  def test_date
+    key_gen = KeyGenerator.new
+    assert_equal '05-11-2018', key_gen.date
   end
 end
