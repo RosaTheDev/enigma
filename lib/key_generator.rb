55e0f6237df5c55943b16031859d23f4ae87d481
Chris Powell <chris.ian.powell@gmail.com>
2018-05-13 15:13:11 -0600

    finished encryptor tests

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,6 +1,6 @@
 # this is a file about the egnigma machine from the world war the sequal
 class KeyGenerator
-  attr_reader :key
+  attr_reader :rand_key
 
   def initialize
     @rand_key = rand(10_000...99_999)
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -41,49 +41,50 @@ class EncryptorTest < Minitest::Test
     assert_equal [32, 24, 43, 35], key_ints
   end
 
-  # def test_squared_date
-  #   assert_equal 2623283524, @encryptor.square_date
-  # end
-  #
-  # def test_last_four_digits_of_date_squared
-  #   sqr_date  = @encryptor.square_date
-  #   last_four = @encryptor.last_four_digits_of_date_squared(sqr_date)
-  #
-  #   assert_equal ['3', '5', '2', '4'], last_four
-  # end
-  #
-  # def test_final_key_for_encode
-  #   key_array = @encryptor.split_into_four_strings
-  #   key_ints  = @encryptor.strings_to_ints(key_array)
-  #
-  #   sqr_date  = @encryptor.square_date
-  #   last_four = @encryptor.last_four_digits_of_date_squared(sqr_date)
-  #   date_ints = @encryptor.strings_to_ints(last_four)
-  #
-  #   final_code_key = @encryptor.final_key_for_encode(key_ints, date_ints)
-  #
-  #   assert_equal [35, 29, 45, 39], final_code_key
-  # end
-  #
-  # def test_split_message
-  #   expected = ["t", "h", "i", "s", " ", "i", "s", " ",\
-  #               "s", "o", " ", "s", "e", "c", "r", "e", "t",\
-  #               " ", ".", ".", "e", "n", "d", ".", "."]
-  #   actual   = @encryptor.split_message
-  #
-  #   assert_equal expected, actual
-  # end
-  #
-  # def test_encrypt
-  #   key_array = @encryptor.split_into_four_strings
-  #   key_ints  = @encryptor.strings_to_ints(key_array)
-  #
-  #   sqr_date  = @encryptor.square_date
-  #   last_four = @encryptor.last_four_digits_of_date_squared(sqr_date)
-  #   date_ints = @encryptor.strings_to_ints(last_four)
-  #
-  #   message = @encryptor.split_message
-  #   @encryptor.encrypt(message, )
-  # end
+  def test_squared_date
+    assert_equal 2623283524, @encryptor.square_date
+  end
 
+  def test_last_four_digits_of_date_squared
+    sqr_date  = @encryptor.square_date
+    last_four = @encryptor.last_four_digits_of_date_squared(sqr_date)
+
+    assert_equal ['3', '5', '2', '4'], last_four
+  end
+
+  def test_final_key_for_encode
+    key_array = @encryptor.split_into_four_strings
+    key_ints  = @encryptor.strings_to_ints(key_array)
+
+    sqr_date  = @encryptor.square_date
+    last_four = @encryptor.last_four_digits_of_date_squared(sqr_date)
+    date_ints = @encryptor.strings_to_ints(last_four)
+
+    final_code_key = @encryptor.final_key_for_encode(key_ints, date_ints)
+
+    assert_equal [35, 29, 45, 39], final_code_key
+  end
+
+  def test_split_message
+    expected = ["t", "h", "i", "s", " ", "i", "s", " ",\
+                "s", "o", " ", "s", "e", "c", "r", "e", "t",\
+                " ", ".", ".", "e", "n", "d", ".", "."]
+    actual   = @encryptor.split_message
+
+    assert_equal expected, actual
+  end
+
+  def test_encrypt
+    key_array = @encryptor.split_into_four_strings
+    key_ints  = @encryptor.strings_to_ints(key_array)
+
+    sqr_date  = @encryptor.square_date
+    last_four = @encryptor.last_four_digits_of_date_squared(sqr_date)
+    date_ints = @encryptor.strings_to_ints(last_four)
+
+    message = @encryptor.split_message
+    secret_message = @encryptor.encrypt(message, key_ints, date_ints)
+
+    assert_equal "p os6.y oedsa5xep0e.adj.7", secret_message
+  end
 end
