5fd13aa32f14e9b16c4e05de6f0d380d5092e525
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-14 17:15:35 -0600

    added the methods_for_encryptor file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -6,7 +6,7 @@ class Decrypt
               :message,
               :date,
               :characters
-              #message and key are nil because in the spec they are not required and if you do not assign them a value they return an error
+
   def initialize(message = nil, key = nil,\
                  date = Date.today.strftime('%m%d%y'))
     @message    = message
@@ -21,22 +21,15 @@ class Decrypt
   end
 
   def decryptor
-
-    encoded = []
-
-    message_enum = split_message.to_enum
-
-    final_key = final_key_for_encode(key_ints, date_ints)
-
-    ##change vairable names from single letter variables to something more clear "char_rotate"
-    # require'pry';binding.pry
-    loop do
-      letter = message_enum.next
-      #changed this variable to a negitive because we are reversing the key in order to decrypt it
-      x = @characters.rotate(@characters.index(letter) - final_key[0])
-      encoded << x[0]
-      final_key.rotate!
-    end
-    encoded.join
+      encoded = []
+      message_enum = split_message.to_enum
+      final_key = final_key_for_encode(key_ints, date_ints)
+      loop do
+        letter = message_enum.next
+        char_rotate = @characters.rotate(@characters.index(letter) - final_key[0])
+        encoded << char_rotate[0]
+        final_key.rotate!
+      end
+      encoded.join
   end
 end
