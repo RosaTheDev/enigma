49eccbd52cde6cd4d23aa1560439a25b3aa3fe22
Chris Powell <chris.ian.powell@gmail.com>
2018-05-16 17:07:30 -0700

    encryptor done

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -19,16 +19,29 @@ class Encryptor
                    '9', ' ', '.', ',']
   end
 
-  def encryptor
-  encoded = []
-  message_enum = split_message.to_enum
-  final_key = final_key_for_encode(key_ints, date_ints)
-  loop do
-    letter = message_enum.next
-    char_rotate = @characters.rotate(@characters.index(letter) + final_key[0])
-    encoded << char_rotate[0]
-    final_key.rotate!
+  def encryptor_loop(message_enum, final_key)
+    encoded = []
+    loop do
+      letter = message_enum.next
+      char_rotate = @characters.rotate(@characters.index(letter) + final_key[0])
+      encoded << char_rotate[0]
+      final_key.rotate!
+    end
+    encoded.join
   end
-  encoded.join
+
+  def encryptor
+    key_array      = split_into_four_strings
+    key_ints       = strings_to_ints(key_array)
+
+    sqr_date       = square_date
+    last_four      = last_four_digits_of_date_squared(sqr_date)
+    date_ints      = strings_to_ints(last_four)
+
+    final_key      = final_key_for_encode(key_ints, date_ints)
+
+    message_enum   = split_message.to_enum
+
+    return encryptor_loop(message_enum, final_key)
   end
 end
